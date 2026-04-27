b748d69f2740e1e9c3c8b8e12f69b0aeb1db6c3c
aayush saxena <saxenapoornima290@gmail.com>
2026-04-27 15:29:23 +0530

    add commands to remove patch and delete end points

--- src/main/java/com/sprint/Repository/CustomerRepository.java
+++ src/main/java/com/sprint/Repository/CustomerRepository.java
@@ -3,7 +3,22 @@ package com.sprint.Repository;
 import com.sprint.Entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(collectionResourceRel = "customers", path = "customers")
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long id);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Customer entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Customer> S save(S entity);
+
+    
 }
