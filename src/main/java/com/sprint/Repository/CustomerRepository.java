2bb61013f4a86e272736b509c47f42ce02c5eb17
aayush saxena <saxenapoornima290@gmail.com>
2026-04-27 15:48:20 +0530

    aded methods in repo

--- src/main/java/com/sprint/Repository/CustomerRepository.java
+++ src/main/java/com/sprint/Repository/CustomerRepository.java
@@ -1,7 +1,14 @@
 package com.sprint.Repository;
 
 import com.sprint.Entities.Customer;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;          
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -20,5 +27,11 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     @RestResource(exported = false)
     <S extends Customer> S save(S entity);
 
-    
+    List<Customer> findByFirstNameAndLastName(
+        @Param("firstName") String firstName,
+        @Param("lastName") String lastName);
+
+    Optional<Customer> findByEmail(@Param("email") String email);
+
+    Page<Customer> findByActive(@Param("active") Boolean active, Pageable pageable);
 }
